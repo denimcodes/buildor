53011fbceec4c7916d31b92976df683d7b820bef
Denim Basumatary <denimbasumatary@outlook.com>
2022-12-05 12:27:33 +0530

    init app

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,24 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { GlowWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { FC, ReactNode, useMemo } from "react";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const url = useMemo(() => clusterApiUrl("devnet"), []);
+  const glow = new GlowWalletAdapter();
+
+  return (
+    <ConnectionProvider endpoint={url}>
+      <WalletProvider wallets={[glow]}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
