fd74e602ed19bb78648a87ee4ab14f6b71bbb436
Denim Basumatary <denimbasumatary@outlook.com>
2022-12-09 16:33:53 +0530

    fix signature verification error

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -3,18 +3,18 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { GlowWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import { FC, ReactNode, useMemo } from "react";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const url = useMemo(() => clusterApiUrl("devnet"), []);
-  const glow = useMemo(() => new GlowWalletAdapter(), []);
+  const phantom = useMemo(() => new PhantomWalletAdapter(), []);
 
   return (
     <ConnectionProvider endpoint={url}>
-      <WalletProvider wallets={[glow]} autoConnect={true}>
+      <WalletProvider wallets={[phantom]} autoConnect={true}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
